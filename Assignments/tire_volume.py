0bb2ced1df175000e0a8cbd55aaaa9dd9c44aeba
Darth-Volix <wil22159@byui.edu>
2024-01-17 13:43:28 -0700

    added code to print the results of the tire volume program to a text file

--- Assignments/tire_volume.py
+++ Assignments/tire_volume.py
@@ -2,12 +2,18 @@
 01/10/2024
 CSE 111-07
 Nicholas Wilkins 
-
+Assignment 1:
 Calculating Tire Volume
 '''
 
 # to begin, import the math library so that pi can be used 
 import math 
+
+# Import the datetime class from the datetime module so that it can be used in this program.
+from datetime import datetime
+
+# Have the program retrieve the current date and time from the system
+current_date = datetime.now()
 
 # next, set the variables and inputs 
 tire_width = float(input('\nEnter the width of the tire as a whole number in mm (ex. 225): '))
@@ -19,3 +25,7 @@ tire_volume = (math.pi * tire_width ** 2 * aspect_ratio * (tire_width * aspect_r
 
 # print the result of the calculation
 print(f'\nThe approximate volume is {tire_volume:.2f} liters.\n')
+
+# open the volumes file and add the outputs of the program as a single line to the file
+with open("volumes.txt", "at") as volumes_file:
+    print(current_date, tire_width, aspect_ratio, wheel_diameter, tire_volume, file=volumes_file)
